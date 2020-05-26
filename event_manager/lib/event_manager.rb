0ab2668280ab71a506bb44d4552973b14abb05ca
Halee Pagel <halee.pagel@gmail.com>
2020-05-26 17:05:09 +0900

    worked with an erb file for the first time

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -253,9 +253,184 @@
 #     puts "#{name} #{zipcode} #{legislators_string}"
 # end
 
-# SHOWING ALL LEGISLATORS IN A ZIP CODE FIRST AND LAST NAMES ONLY, WITH EXCEPTIONS FOR WRONG ZIP CODE
+# # SHOWING ALL LEGISLATORS IN A ZIP CODE FIRST AND LAST NAMES ONLY, WITH EXCEPTIONS FOR WRONG ZIP CODE
+# require "csv"
+# require "google/apis/civicinfo_v2"
+
+# def clean_zipcode(zipcode)
+#     zipcode.to_s.rjust(5, "0")[0..4]
+# end
+
+# def legislators_by_zipcode(zip)
+#     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#     civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+
+#     begin
+#     legislators = civic_info.representative_info_by_address(
+#         address: zip,
+#         levels: "country",
+#         roles: ["legislatorUpperBody", "legislatorLowerBody"])
+#     legislators = legislators.officials
+#     legislator_names = legislators.map(&:name)
+#     legislator_names.join(", ")
+#     rescue
+#     "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+#     end
+# end
+
+# puts "EventManager Initialized!"
+
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+# contents.each do |row|
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+
+#     puts "#{name} #{zipcode} #{legislators}"
+# end
+
+# FORM LETTERS, FROM ATTENDEES TO REPRESENTATIVES
+
+# # REPLACING WITH GSUB AND GSUB!
+# # gsub - returns a new copy of the original string with the values replaced
+# # gsub! - replaces the values in the original string
+
+# require "csv"
+# require "google/apis/civicinfo_v2"
+
+# def clean_zipcode(zipcode)
+#     zipcode.to_s.rjust(5, "0")[0..4]
+# end
+
+# def legislators_by_zipcode(zip)
+#     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#     civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+    
+#     begin
+#         legislators = civic_info.representative_info_by_address(
+#             address: zip,
+#             levels: "country",
+#             roles: ["legislatorUpperBody", "legislatorLowerBody"])
+#             legislators = legislators.officials
+#             legislator_names = legislators.map(&:name)
+#             legislator_names.join(", ")
+#         rescue
+#             "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+#         end
+#     end
+    
+# puts "EventManager Initialized!"
+    
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+    
+# template_letter = File.read "form_letter.html"
+
+# contents.each do |row|
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+
+#     personal_letter= template_letter.gsub("FIRST_NAME",name)
+#     personal_letter= template_letter.gsub!("LEGISLATORS",legislators)
+
+#     puts personal_letter
+# end
+
+# # RUBY'S ERB - A TEMPLATE LANGUAGE
+
+# require "csv"
+# require "google/apis/civicinfo_v2"
+# require "erb"
+
+# def clean_zipcode(zipcode)
+#     zipcode.to_s.rjust(5, "0")[0..4]
+# end
+
+# def legislators_by_zipcode(zip)
+#     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#     civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+    
+#     begin
+#         legislators = civic_info.representative_info_by_address(
+#             address: zip,
+#             levels: "country",
+#             roles: ["legislatorUpperBody", "legislatorLowerBody"]
+#         ).officials
+#         rescue
+#             "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+#         end
+#     end
+    
+# puts "EventManager Initialized!"
+    
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+    
+# template_letter = File.read "form_letter.erb"
+# erb_template = ERB.new template_letter
+
+# contents.each do |row|
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+#     form_letter = erb_template.result(binding)
+
+#     puts form_letter
+# end
+
+# # OUTPUTTING FORM LETTERS TO A FILE
+
+# require "csv"
+# require "google/apis/civicinfo_v2"
+# require "erb"
+
+# def clean_zipcode(zipcode)
+#     zipcode.to_s.rjust(5, "0")[0..4]
+# end
+
+# def legislators_by_zipcode(zip)
+#     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#     civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+    
+#     begin
+#         legislators = civic_info.representative_info_by_address(
+#             address: zip,
+#             levels: "country",
+#             roles: ["legislatorUpperBody", "legislatorLowerBody"]
+#         ).officials
+#         rescue
+#             "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+#         end
+#     end
+    
+# puts "EventManager Initialized!"
+    
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+    
+# template_letter = File.read "form_letter.erb"
+# erb_template = ERB.new template_letter
+
+# contents.each do |row|
+#     id = row[0]
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+#     form_letter = erb_template.result(binding)
+
+#    Dir.mkdir("output") unless Dir.exists? "output"
+
+#    filename = "output/thanks_#{id}.html"
+
+#    File.open(filename, "w") do |file|
+#     file.puts form_letter
+#    end
+# end
+
+# MOVING FORM LETTER GENERATION TO A METHOD
+
 require "csv"
 require "google/apis/civicinfo_v2"
+require "erb"
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, "0")[0..4]
@@ -264,30 +439,40 @@ end
 def legislators_by_zipcode(zip)
     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
     civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
-
+    
     begin
-    legislators = civic_info.representative_info_by_address(
-        address: zip,
-        levels: "country",
-        roles: ["legislatorUpperBody", "legislatorLowerBody"])
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
-    rescue
-    "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+        legislators = civic_info.representative_info_by_address(
+            address: zip,
+            levels: "country",
+            roles: ["legislatorUpperBody", "legislatorLowerBody"]
+        ).officials
+        rescue
+            "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+        end
+    end
+
+def save_thank_you_letter(id, form_letter)
+    Dir.mkdir("output") unless Dir.exists?("output")
+
+    filename = "output/thanks_#{id}.html"
+
+    File.open(filename, "w") do |file|
+        file.puts form_letter
     end
 end
-
+    
 puts "EventManager Initialized!"
-
+    
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+    
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
+    id = row[0]
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
     legislators = legislators_by_zipcode(zipcode)
-
-    puts "#{name} #{zipcode} #{legislators}"
+    form_letter = erb_template.result(binding)
+    save_thank_you_letter(id,form_letter)
 end
-
-# FORM LETTERS, FROM ATTENDEES TO REPRESENTATIVES
