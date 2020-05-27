f9b408ebbed48e33fc6dda66feacc1fea0c5945d
Halee Pagel <halee.pagel@gmail.com>
2020-05-27 14:05:05 +0900

    updated form letters

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -426,7 +426,58 @@
 #    end
 # end
 
-# MOVING FORM LETTER GENERATION TO A METHOD
+# # MOVING FORM LETTER GENERATION TO A METHOD
+
+# require "csv"
+# require "google/apis/civicinfo_v2"
+# require "erb"
+
+# def clean_zipcode(zipcode)
+#     zipcode.to_s.rjust(5, "0")[0..4]
+# end
+
+# def legislators_by_zipcode(zip)
+#     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+#     civic_info.key = "AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw"
+    
+#     begin
+#         legislators = civic_info.representative_info_by_address(
+#             address: zip,
+#             levels: "country",
+#             roles: ["legislatorUpperBody", "legislatorLowerBody"]
+#         ).officials
+#         rescue
+#             "You can find your legislators by visiting www.commoncause.org/take-action/find-elected-officials"
+#         end
+#     end
+
+# def save_thank_you_letter(id, form_letter)
+#     Dir.mkdir("output") unless Dir.exists?("output")
+
+#     filename = "output/thanks_#{id}.html"
+
+#     File.open(filename, "w") do |file|
+#         file.puts form_letter
+#     end
+# end
+    
+# puts "EventManager Initialized!"
+    
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+    
+# template_letter = File.read "form_letter.erb"
+# erb_template = ERB.new template_letter
+
+# contents.each do |row|
+#     id = row[0]
+#     name = row[:first_name]
+#     zipcode = clean_zipcode(row[:zipcode])
+#     legislators = legislators_by_zipcode(zipcode)
+#     form_letter = erb_template.result(binding)
+#     save_thank_you_letter(id,form_letter)
+# end
+
+# CLEAN UP PHONE NUMBERS
 
 require "csv"
 require "google/apis/civicinfo_v2"
@@ -434,6 +485,12 @@ require "erb"
 
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def clean_phone_numbers(phone_number)
+    phone_number.to_s
+    phone_number.sub("^0-9", "" )
+    puts phone_number
 end
 
 def legislators_by_zipcode(zip)
@@ -472,6 +529,7 @@ contents.each do |row|
     id = row[0]
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
+    phone = clean_phone_numbers(row[:homephone])
     legislators = legislators_by_zipcode(zipcode)
     form_letter = erb_template.result(binding)
     save_thank_you_letter(id,form_letter)
